5f948c097d31ac801ee5b5ca4f48bb4f104f9e3c
Liam <liam@liamco.io>
2025-10-12 12:28:10 +0100

    tidy cc extractor

--- src/cc_funder_data_extractor.py
+++ src/cc_funder_data_extractor.py
@@ -12,42 +12,54 @@ api_key = os.getenv("API_KEY")
 
 def get_funder_data():
 
-	#define api variables
-	hdr ={
-		"Cache-Control": "no-cache",
-		"Ocp-Apim-Subscription-Key": f"{api_key}",
-		}
-	operation = ["allcharitydetailsV2", "charityoverview", "charitygoverningdocument"]
-	# c_nums, _ = get_sample()
-	c_nums = ["1015792", "1168435", "1113488"]
-	charity_data = {}
-	columns = ["reg_charity_number", "charity_name", "web", "activities", "charitable_objects", "latest_income", "latest_expenditure", "who_what_where", "CharityAoOCountryContinent", "CharityAoOLocalAuthority", "CharityAoORegion"]
-	df_rows = []
-
-	#call api
-	df = call_cc_api(operation, hdr, c_nums, charity_data, columns, df_rows)
-
-	#rename columns to match schema
-	df = df.rename(columns={
-		"reg_charity_number": "registered_num",
-		"charity_name": "name",
-		"web": "website",
-		"charitable_objects": "objectives",
-		"latest_income": "income",
-		"latest_expenditure": "expenditure",
-		"who_what_where": "classifications",
-		"CharityAoOCountryContinent": "country_continent",
-		"CharityAoOLocalAuthority": "local_authority",
-		"CharityAoORegion": "region"
-	})
-
-	#build funders table
-	funders = df.drop(columns=["classifications", "country_continent", "local_authority", "region"])
-
-	#build beneficiaries and causes tables and join tables
-	beneficiaries, funder_beneficiaries, causes, funder_causes = build_classifications_tables(df)
+	"""
+		Gets data from the Charity Commission API.
+		Returns dataframes to be piped to database, containing information about funders, the areas where they work, and the beneficiaries and causes that they support.
+		Also builds join tables to deal with many-to-many relationships in the database.
+		Also returns list of charity numbers used to fetch the data.
+	"""
 	
-	#build areas table and join table
-	funder_areas, areas = build_areas_tables(df)	
+	try:
+		#define api variables
+		hdr ={
+			"Cache-Control": "no-cache",
+			"Ocp-Apim-Subscription-Key": f"{api_key}",
+			}
+		operation = ["allcharitydetailsV2", "charityoverview", "charitygoverningdocument"]
+		# c_nums, _ = get_sample()
+		c_nums = ["1015792", "1168435", "239754", "265281", "287535", "1185673", "1197528", "1051202"]
+		charity_data = {}
+		columns = ["reg_charity_number", "charity_name", "web", "activities", "charitable_objects", "latest_income", "latest_expenditure", "who_what_where", "CharityAoOCountryContinent", "CharityAoOLocalAuthority", "CharityAoORegion"]
+		df_rows = []
 
-	return funders, funder_beneficiaries, beneficiaries, funder_causes, causes, funder_areas, areas, c_nums
+		#call api
+		df = call_cc_api(operation, hdr, c_nums, charity_data, columns, df_rows)
+
+		#rename columns to match schema
+		df = df.rename(columns={
+			"reg_charity_number": "registered_num",
+			"charity_name": "name",
+			"web": "website",
+			"charitable_objects": "objectives",
+			"latest_income": "income",
+			"latest_expenditure": "expenditure",
+			"who_what_where": "classifications",
+			"CharityAoOCountryContinent": "country_continent",
+			"CharityAoOLocalAuthority": "local_authority",
+			"CharityAoORegion": "region"
+		})
+
+		#build funders table
+		funders = df.drop(columns=["classifications", "country_continent", "local_authority", "region"])
+
+		#build beneficiaries and causes tables and join tables
+		beneficiaries, funder_beneficiaries, causes, funder_causes = build_classifications_tables(df)
+		
+		#build areas table and join table
+		funder_areas, areas = build_areas_tables(df)	
+
+		return funders, funder_beneficiaries, beneficiaries, funder_causes, causes, funder_areas, areas, c_nums
+	
+	except Exception as e:
+		print(f"Error in get_funder_data: {e}")
+		raise
