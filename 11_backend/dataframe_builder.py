39b000f845d6ca4510e496af1b22c867093a07a0
Liam <liam@liamco.io>
2025-12-29 20:52:59 +0000

    build grants_df enricher

--- 11_backend/dataframe_builder.py
+++ 11_backend/dataframe_builder.py
@@ -6,6 +6,7 @@ from supabase import create_client
 from dotenv import load_dotenv
 import os
 import sys
+from collections import defaultdict
 from scoring_logic import calculate_alignment_score
 
 #add project root to path
@@ -130,7 +131,6 @@ def build_grants_df(funder_number, url, key):
 
             for i in range(0, len(all_grant_ids), batch_size):
                 batch_ids = all_grant_ids[i:i+batch_size]
-                batch_num = (i // batch_size) + 1
                 grants_response = supabase.table("grants").select("*").in_("grant_id", batch_ids).execute()
                 all_grants.extend(grants_response.data)
 
@@ -148,16 +148,130 @@ def build_grants_df(funder_number, url, key):
 grants_df = build_grants_df(test_funder_number, url, key)
 
 def enrich_grants_df(grants_df, url, key):
-
-
-
+    """
+    Gets recipient data to enrich the grants dataframe.
+    """
 
     supabase = create_client(url, key)
 
     if len(grants_df) > 0:
-        print(f"\nEnriching grants with recipient data...")
         grant_ids_list = grants_df["grant_id"].tolist()
 
+        #join grants to recipients
+        all_recipient_joins = []
+        offset = 0
+        batch_size = 1000
+
+        while True:
+            recipient_joins = supabase.table("recipient_grants").select("grant_id, recipient_id").in_("grant_id", grant_ids_list).range(offset, offset + batch_size - 1).execute()
+
+            if not recipient_joins.data:
+                break
+
+            all_recipient_joins.extend(recipient_joins.data)
+            if len(recipient_joins.data) < batch_size:
+                break
+
+            offset += batch_size
+
+        #map grants to unique recipients
+        grant_to_recipient = {join["grant_id"]: join["recipient_id"] for join in all_recipient_joins}
+        recipient_ids = list(set(grant_to_recipient.values()))
+
+        #fetch recipient data
+        all_recipients = []
+        batch_size = 100
+
+        for i in range(0, len(recipient_ids), batch_size):
+            batch_ids = recipient_ids[i:i+batch_size]
+            fetched_recipients = supabase.table("recipients").select("*").in_("recipient_id", batch_ids).execute()
+            all_recipients.extend(fetched_recipients.data)
+
+        recipients_df = pd.DataFrame(all_recipients)
+
+        #get classifications for reecipients
+        if len(all_recipients) > 0:
+            recipient_ids_list = [r["recipient_id"] for r in all_recipients]
+
+            #areas
+            recipient_area_joins = []
+            offset = 0
+            while True:
+                joins = supabase.table("recipient_areas").select("recipient_id, area_id").in_("recipient_id", recipient_ids_list).range(offset, offset + 999).execute()
+                if not joins.data:
+                    break
+                recipient_area_joins.extend(joins.data)
+                if len(joins.data) < 1000:
+                    break
+                offset += 1000
+
+            #beneficiaries
+            recipient_ben_joins = []
+            offset = 0
+            while True:
+                joins = supabase.table("recipient_beneficiaries").select("recipient_id, ben_id").in_("recipient_id", recipient_ids_list).range(offset, offset + 999).execute()
+                if not joins.data:
+                    break
+                recipient_ben_joins.extend(joins.data)
+                if len(joins.data) < 1000:
+                    break
+                offset += 1000
+
+            #causes
+            recipient_cause_joins = []
+            offset = 0
+            while True:
+                joins = supabase.table("recipient_causes").select("recipient_id, cause_id").in_("recipient_id", recipient_ids_list).range(offset, offset + 999).execute()
+                if not joins.data:
+                    break
+                recipient_cause_joins.extend(joins.data)
+                if len(joins.data) < 1000:
+                    break
+                offset += 1000
+
+            #get tables for lookups
+            beneficiaries_df = get_table_from_supabase(url, key, "beneficiaries")
+            causes_df = get_table_from_supabase(url, key, "causes")
+            areas_lookup_df = get_table_from_supabase(url, key, "areas")
+            area_lookup = dict(zip(areas_lookup_df["area_id"], areas_lookup_df["area_name"]))
+            ben_lookup = dict(zip(beneficiaries_df["ben_id"], beneficiaries_df["ben_name"]))
+            cause_lookup = dict(zip(causes_df["cause_id"], causes_df["cause_name"]))
+
+            #group by id
+            recipient_areas_map = defaultdict(list)
+            recipient_bens_map = defaultdict(list)
+            recipient_causes_map = defaultdict(list)
+
+            for join in recipient_area_joins:
+                area_name = area_lookup.get(join["area_id"])
+                if area_name:
+                    recipient_areas_map[join["recipient_id"]].append(area_name)
+
+            for join in recipient_ben_joins:
+                ben_name = ben_lookup.get(join["ben_id"])
+                if ben_name:
+                    recipient_bens_map[join["recipient_id"]].append(ben_name)
+
+            for join in recipient_cause_joins:
+                cause_name = cause_lookup.get(join["cause_id"])
+                if cause_name:
+                    recipient_causes_map[join["recipient_id"]].append(cause_name)
+
+            #add classifications to df
+            recipients_df["recipient_areas"] = recipients_df["recipient_id"].map(lambda x: recipient_areas_map.get(x, []))
+            recipients_df["recipient_beneficiaries"] = recipients_df["recipient_id"].map(lambda x: recipient_bens_map.get(x, []))
+            recipients_df["recipient_causes"] = recipients_df["recipient_id"].map(lambda x: recipient_causes_map.get(x, []))
+
+        #join recipients to grants
+        grants_df["recipient_id"] = grants_df["grant_id"].map(grant_to_recipient)
+        grants_df = grants_df.merge(
+            recipients_df,
+            left_on="recipient_id",
+            right_on="recipient_id",
+            how="left"
+        )
+
+    grants_df["funder_num"] = test_funder_number
 
     return grants_df
 
